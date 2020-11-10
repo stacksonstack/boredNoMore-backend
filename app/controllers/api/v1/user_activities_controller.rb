f7054e550876ff54c7006cda60ab569124036e92
stacksonstack <staceycarrillo44@gmail.com>
2020-11-10 16:45:43 -0500

    finished controller for user_activities

--- app/controllers/api/v1/user_activities_controller.rb
+++ app/controllers/api/v1/user_activities_controller.rb
@@ -28,14 +28,23 @@ class Api::V1::UserActivitiesController < ApplicationController
         end
     end
 
-
+    def destroy
+        userActivity = UserActivity.find(params[:id])
+        if userActivity.destroy
+            render json: userActivity
+        #     flash[:success] = 'Object was successfully deleted.'
+        #     redirect_to objects_url
+        # else
+        #     flash[:error] = 'Something went wrong'
+        #     redirect_to objects_url
+        end
+    end
+    
     private
 
     def userActivity_params
         params.require(:user_activities).permit(:user_id, :activity_id)
     end
-    
-
 end
 
 
