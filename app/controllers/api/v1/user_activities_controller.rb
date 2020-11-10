af5cba51e4fa7f581c48e51c98d2bc8cc617e0d7
stacksonstack <staceycarrillo44@gmail.com>
2020-11-10 13:21:42 -0500

    worked on userActivities controller

--- app/controllers/api/v1/user_activities_controller.rb
+++ app/controllers/api/v1/user_activities_controller.rb
@@ -1,2 +1,41 @@
 class Api::V1::UserActivitiesController < ApplicationController
+    
+    def show
+        userActivity = UserActivity.find(params[:id])
+        render json: userActivity
+    end
+
+    def index
+        userActivities = UserActivity.all
+        render json: userActivities
+    end
+
+    def new
+        userActivity = UserActivity.new
+        
+    end
+    
+    def create
+        userActivity = UserActivity.create(userActivity_params)
+        if userActivity.valid?
+        #   flash[:success] = "UserActivity successfully created"
+          render json: userActivity
+        #   redirect_to userActivity
+        # else
+        #   flash[:error] = "Something went wrong"
+        #   
+        #   render 'new'
+        end
+    end
+
+
+    private
+
+    def userActivity_params
+        params.require(:user_activities).permit(:user_id, :activity_id)
+    end
+    
+
 end
+
+
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::UsersController < ApplicationController
 
     def show
-        users = User.find(params[:id])
-        render json: users
+        user = User.find(params[:id])
+        render json: user
     end
 
     def index
-        user = User.all
-        render json: user
+        users = User.all
+        render json: users
     end
     
     
